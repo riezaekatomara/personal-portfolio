b536c5bb96cd4c2dd5084d6bcef297789629a81f
Rieza Eka Tomara <riezaekatomara@gmail.com>
2025-04-29 15:23:54 +0700

    add picture personal-portfolio.png to assets/png and update link in DataPortfolio.jsx

--- src/data/DataPortfolio.jsx
+++ src/data/DataPortfolio.jsx
@@ -1,5 +1,5 @@
 import portfolio1 from "../assets/png/tickitz-home.png";
-import portfolio2 from "../assets/png/portfolio2.png";
+import portfolio2 from "../assets/png/personal-portfolio.png";
 import portfolio3 from "../assets/png/portfolio3.png";
 import portfolio4 from "../assets/png/portfolio4.png";
 import portfolio5 from "../assets/png/portfolio5.png";
@@ -9,31 +9,31 @@ export const portfolioList = [
   {
     id: "1",
     image: portfolio1,
-    link: "https://tickitz-rieza.vercel.app/",
+    link: "https://tickitz-rieza.vercel.app",
   },
   {
     id: "2",
     image: portfolio2,
-    link: "https://tickitz-rieza.vercel.app/",
+    link: "https://personal-portfolio-rieza.vercel.app",
   },
   {
     id: "3",
     image: portfolio3,
-    link: "https://tickitz-rieza.vercel.app/",
+    link: "https://personal-portfolio-rieza.vercel.app",
   },
   {
     id: "4",
     image: portfolio4,
-    link: "https://tickitz-rieza.vercel.app/",
+    link: "https://personal-portfolio-rieza.vercel.app",
   },
   {
     id: "5",
     image: portfolio5,
-    link: "https://tickitz-rieza.vercel.app/",
+    link: "https://personal-portfolio-rieza.vercel.app",
   },
   {
     id: "6",
     image: portfolio6,
-    link: "https://tickitz-rieza.vercel.app/",
+    link: "https://personal-portfolio-rieza.vercel.app",
   },
 ];
